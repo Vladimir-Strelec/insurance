b578a38bc559423b1d28df372748aac5dcaa80a0
Vladimir-Strelec <vovik050github@gmail.com>
2025-05-16 19:23:48 +0200

    Deploy 5

--- insuranceProject/myproject/insurance/urls.py
+++ insuranceProject/myproject/insurance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import InsuranceAjaxView, get_subcategories, submit_lead_view, subcategory_detail
 
 urlpatterns = [
-    path('insurance-dynamic/', InsuranceAjaxView.as_view(), name='insurance_dynamic'),
+    path('', InsuranceAjaxView.as_view(), name='insurance_dynamic'),
     path('submit-lead/', submit_lead_view, name='submit_lead'),
     path('get-subcategories/<int:category_id>/', get_subcategories, name='get_subcategories'),
     path('subcategory/<int:main_id>/<int:sub_id>/', subcategory_detail, name='subcategory_detail'),
