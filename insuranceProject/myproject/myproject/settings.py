352df31c41949e5e20abc57da0c1425312de93b0
Vladimir-Strelec <vovik050github@gmail.com>
2025-05-17 16:35:22 +0200

    Deploy 12

--- insuranceProject/myproject/myproject/settings.py
+++ insuranceProject/myproject/myproject/settings.py
@@ -87,7 +87,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://your-static-site.onrender.com/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
