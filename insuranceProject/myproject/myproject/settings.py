8d48f1d3b25a723fdac4523d67e316c776f1d607
Vladimir-Strelec <vovik050github@gmail.com>
2025-05-17 13:59:23 +0200

    Deploy 11

--- insuranceProject/myproject/myproject/settings.py
+++ insuranceProject/myproject/myproject/settings.py
@@ -56,12 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#}
 DATABASES = {
     "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL"),
