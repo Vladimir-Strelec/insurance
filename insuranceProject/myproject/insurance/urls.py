8e368791b3142377d06e8b432c1837728d6b4a5a
Vladimir-Strelec <vovik050github@gmail.com>
2025-06-01 21:08:10 +0200

    Fix seo and preload

--- insuranceProject/myproject/insurance/urls.py
+++ insuranceProject/myproject/insurance/urls.py
@@ -12,7 +12,7 @@ sitemaps = {
 
 urlpatterns = [
     path('', InsuranceAjaxView.as_view(), name='insurance_dynamic'),
-    path('submit-lead/', submit_lead_view, name='submit_lead'),
+    # path('submit-lead/', submit_lead_view, name='submit_lead'),
     path('get-subcategories/<int:category_id>/', get_subcategories, name='get_subcategories'),
     path('subcategory/<int:main_id>/<int:sub_id>/', subcategory_detail, name='subcategory_detail'),
     path('policy/', get_policy, name='policy'),
