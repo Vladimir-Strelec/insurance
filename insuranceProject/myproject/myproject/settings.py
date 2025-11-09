ae0bef864b3e2c375aa557787d704631cb70f680
Vladimir-Strelec <vovik050github@gmail.com>
2025-11-09 19:51:19 +0100

    Fix backend 1.6

--- insuranceProject/myproject/myproject/settings.py
+++ insuranceProject/myproject/myproject/settings.py
@@ -103,6 +103,13 @@ DATABASES = {
     )
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 LANGUAGE_CODE = "de"
 TIME_ZONE = "UTC"
 USE_I18N = True
